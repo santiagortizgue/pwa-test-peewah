7df67547286469966d63fad0ee9290a9268591a8
Sebastian Castaño Jara <sebastian.caja@gmail.com>
2020-01-21 16:06:09 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Peewah.png';
 import './App.css';
 
 const App: React.FC = () => {
@@ -8,16 +8,8 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          ¡Hola, esta es una PWA para Peewah!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
